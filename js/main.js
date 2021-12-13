4da76d410d5200e686ea51ef55ba64a230c55274
Chris Chu <christopherjc12@gmail.com>
2021-12-12 20:29:47 -0800

    cache DOM elements. rename ids

--- js/main.js
+++ js/main.js
@@ -5,7 +5,18 @@
 
 
 // Cached DOM Variables
+const difficultyEls = document.querySelectorAll('input');
 
+const playerHPEl = document.querySelector('#playerHP');
+const PlayerSTREl = document.querySelector('#playerSTR');
+const monsterHPEl = document.querySelector('#monsterHP');
+const monsterSTREl = document.querySelector('#monsterSTR');
+
+const monsterSpriteEl = document.querySelector('#monsterSprite');
+
+const startEl = document.querySelector('#start');
+const rollEl = document.querySelector('#roll');
+const messageEl = document.querySelector('#message p');
 
 // Init and Render
 
