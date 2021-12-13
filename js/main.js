570bcdb258b258c95f37cedbfc88ca1235cccf2c
Chris Chu <christopherjc12@gmail.com>
2021-12-13 01:01:05 -0800

    add cached DOM elements. add gameInProgress conditional to render function. set hidden attribute css display to none

--- js/main.js
+++ js/main.js
@@ -35,8 +35,8 @@ const monsters = [
     }
 ];
 
-// State Variables
 
+// State Variables
 let turn;
 let gameInProgress;
 let roundsSurvived;
@@ -47,39 +47,62 @@ let monsterHP;
 let currentMonster;
 
 let rollNum;
+let message;
+
 
 // Cached DOM Variables
 const difficultyEls = document.querySelectorAll('input');
+const difficultyDivEl = document.querySelector('#difficulty');
+const playerStatsEl = document.querySelector('#playerStats');
 
 const playerHPEl = document.querySelector('#playerHP');
 const PlayerSTREl = document.querySelector('#playerSTR');
 const monsterHPEl = document.querySelector('#monsterHP');
 const monsterSTREl = document.querySelector('#monsterSTR');
 
+const monsterDivEl = document.querySelector('.monster');
 const monsterSpriteEl = document.querySelector('#monsterSprite');
 
 const startEl = document.querySelector('#start button');
 const rollEl = document.querySelector('#roll button');
 const messageEl = document.querySelector('#message p');
 
-// Init and Render
 
+// Init and Render
 function init(){
     console.log('Initial state');
     gameInProgress = false;
     roundsSurvived = 0;
+    messageEl.innerText = 'Choose a difficulty \n \n Good luck'
+    playerStatsEl.hidden = true;
+    difficultyDivEl.hidden = false;
+    monsterDivEl.hidden = true;
 };
 
 init();
 
 function render(){
     console.log('Wrender');
+    if (gameInProgress === true){
+        startEl.disabled = true;
+        rollEl.disabled = false;
+        monsterDivEl.hidden = false;
+        playerStatsEl.hidden = false;
+        difficultyDivEl.hidden = true;
+    } else {
+        startEl.disabled = false;
+        rollEl.disabled = true;
+        difficultyDivEl.hidden = false;
+        playerStatsEl.hidden = true;
+        monsterDivEl.hidden = true;
+    }
+
     playerHPEl.innerText = playerHP;
     PlayerSTREl.innerText = playerSTR;
 };
 
-// Event Listeners
 
+// Event Listeners
 startEl.addEventListener('click', function(event){
     if (startEl.disabled === false){
         difficultyEls.forEach(function(option){
@@ -88,12 +111,13 @@ startEl.addEventListener('click', function(event){
                 playerSTR = playerStats[option.value].STR;
             };
         });
+        gameInProgress = true;
     };
     render();
 });
 
-// Functions
 
+// Functions
 function chooseMonster(array){
     currentMonster = array[Math.floor(Math.random()*array.length)];
 };
